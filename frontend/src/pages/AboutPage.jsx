3b89a1d8fe630a3c3bf64a58f75d573871fe7fd4
dengoan969 <dengoan969@gmail.com>
2026-04-10 21:24:34 +0500

    lint files 2

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,16 +1,16 @@
-    import React from 'react';
-    import '../styles/AboutPage.css';
+import React from "react";
+import "../styles/AboutPage.css";
 
-    const AboutPage = () => {
-        return (
-            <div className="about-wrapper">
-                <div className="about-container">
-                    <div className="about-header">
-                        <div className="about-title">О санатории</div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
+const AboutPage = () => {
+  return (
+    <div className="about-wrapper">
+      <div className="about-container">
+        <div className="about-header">
+          <div className="about-title">О санатории</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    export default AboutPage;
+export default AboutPage;
